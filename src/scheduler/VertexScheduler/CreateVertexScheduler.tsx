101674e9a511f011c3593d23bec528e81a7e5328
vysakhviswama <vysakhma@google.com>
2024-12-02 10:48:34 +0000

    Add Paramter button visibile initially and  key and value input is hidden

--- src/scheduler/VertexScheduler/CreateVertexScheduler.tsx
+++ src/scheduler/VertexScheduler/CreateVertexScheduler.tsx
@@ -457,7 +457,6 @@ const CreateVertexScheduler = ({
                                 setValueValidation={setValueValidation}
                                 duplicateKeyError={duplicateKeyError}
                                 setDuplicateKeyError={setDuplicateKeyError}
-                                fromPage="scheduler"
                             />
                         </>
 
